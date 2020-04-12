ae07295d61a638918d119164215dbad7943691f6
Florent Bouisset <florent.bouisset@gmail.com>
2020-04-13 01:44:14 +0200

    correction des accents

--- conversionXMLtoHTML.py
+++ conversionXMLtoHTML.py
@@ -1,11 +1,17 @@
 import lxml.etree as ET
 import os
-dom = ET.parse("structure.xml")
+
+pathInputXML = "structure.xml"
+pathInputXSLT = "transform.xml"
+pathOutputHTML = "cartegrise.html"
+
+dom = ET.parse(pathInputXML)
 xslt = ET.parse("transform.xml")
 transform = ET.XSLT(xslt)
 newdom = transform(dom)
-os.remove("cartegrise.html")
-f= open("cartegrise.html","x")
+if (os.path.exists(pathOutputHTML)):
+    os.remove(pathOutputHTML)
+f= open(pathOutputHTML,encoding = 'utf-8',mode = 'x')
 f.write(str(newdom))
 f.close()
 print(ET.tostring(newdom, pretty_print=True))
